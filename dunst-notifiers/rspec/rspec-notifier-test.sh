aa271b4d2642de0fc5d18c3ad4608e75bc418c84
Juraci <juraci.vieira@gmail.com>
2021-11-07 23:26:59 -0300

    instroducing dunst-notifiers

---
+++ dunst-notifiers/rspec/rspec-notifier-test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rspec-notifier echo "90 examples, 0 failures, 6 pending"
+rspec-notifier echo "90 examples, 0 failures"
+rspec-notifier echo "90 examples, 1 failure"
+rspec-notifier echo "90 examples, 10 failures"
+rspec-notifier echo "1 error occurred outside of examples"
+rspec-notifier echo "what is this"
